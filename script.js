76d35643fa08707b48e998f69b88adc98118d100
Luiza Souza <angelaluizapereira679@gmail.com>
2023-09-16 14:57:46 -0300

    add update

---
+++ script.js
@@ -0,0 +1,23 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+const inputColor = document.querySelector(".input_color");
+const tools = document.querySelectorAll(".button_tool");
+const sizeButtons = document.querySelector(".button_size");
+const ButtonClear = document.querySelector(".button_clear");
+
+let brushSize = 50;
+ctx.fillStyle = "#000";
+
+canvas.addEventListener("mousedown", (event) => {
+  const { clientX, clientY } = event;
+  draw(clientX, clientY);
+});
+const draw = (x, y) => {
+  ctx.fillRect(
+    x - canvas.offsetLeft,
+    y - canvas.offsetTop,
+    brushSize,
+    brushSize
+  );
+};
